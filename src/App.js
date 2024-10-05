bc52a21cf7a0b58d818e4d6efe719034262f63f6
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2024-10-05 15:53:38 +0300

    feature/counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+// import { useState } from 'react';
+
 import styles from './App.module.css';
+import FriendList from './components/friendList/FriendList.jsx';
 import Profile from './components/profile/Profile.jsx';
 import Statistics from './components/statistics/Statistics.jsx';
-import FriendList from './components/friendList/FriendList.jsx';
-import TransactionHistory from './components/transactionHistory/TransactionHistory.jsx';
-import user from './json/user.json';
+import TransactionHistory
+  from './components/transactionHistory/TransactionHistory.jsx';
 import data from './json/data.json';
 import friends from './json/friends.json';
 import transactions from './json/transactions.json';
+import user from './json/user.json';
+
+// const Comp = () => {
+//   const [value, setValue] = useState(0);
+//   return (
+//     <div>
+//       <span>Incriment{value}</span>
+//       <button type="button" onClick={() => {setValue(value + 1)}}>Click</button>
+//     </div>
+//   )
+// }
 
 export default function App() {
   return (
@@ -21,6 +34,7 @@ export default function App() {
       <Statistics title="Upload stats" data={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
+      {/* <Comp/> */}
     </div>
   );
 }
